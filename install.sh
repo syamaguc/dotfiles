87dd88575ce85d50f2a3ae8f11b4bb777babb12c
syamaguc <programmer2844@gmail.com>
2020-10-27 03:18:48 +0900

    ♻️   Refactor install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,20 @@
+ESSENTIALS='stow \
+            ack \
+            silversercher-ag \
+            neovim \
+            vim-GTK3.0'
+
+ADDTIONALS='fcitx \
+            mozc
+            psensor'
+
 sudo apt update -y && sudo apt upgrade -y
 
-sudo apt install -y stow ack silversearcher-ag
+if [ $1 == "all" ]; then
+    echo -e "\033[0;33minstall all\033[0;39m"
+    sudo apt install -y $ESSENTIAlS $ADDITIONALS
+else
+    echo -e "\033[0;35minstall essentials\033[0;39m"
+    sudo apt install -y $ESSENTIAlS
+fi
 

9923d3329d89863ed235c13777c057e49af66dfd
syamaguc <programmer2844@gmail.com>
2020-10-27 14:11:46 +0900

    ➕  Add slack

--- install.sh
+++ install.sh
@@ -6,9 +6,11 @@ ESSENTIALS='stow \
 
 ADDTIONALS='fcitx \
             mozc \
+            clang \
             psensor'
 
 SNAPS='htop'
+SNAPS_CL='slack'
 
 sudo apt update -y && sudo apt upgrade -y
 sudo snap refresh
@@ -19,6 +21,7 @@ if [ "$1" == "all" ]; then
     echo -e "\033[0;33minstall all\033[0;39m"
     sudo apt install -y $ESSENTIAlS $ADDITIONALS
     sudo snap install $SNAPS
+    sudo snap install $SNAPS_CL --classic
 else
     echo -e "\033[0;35minstall essentials\033[0;39m"
     sudo apt install -y $ESSENTIAlS

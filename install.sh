f9e619625c1364ec198e3ab5cb93d2a900373528
syamaguc <programmer2844@gmail.com>
2020-11-06 15:10:21 +0900

    🐛  Fix install scrpit, single quote to double quote.

--- install.sh
+++ install.sh
@@ -1,16 +1,15 @@
-ESSENTIALS='stow \
+ESSENTIALS="stow \
             ack \
             silversercher-ag \
             neovim \
-            vim-GTK3.0'
+            vim-GTK3.0"
 
-ADDTIONALS='fcitx \
+ADDTIONALS="fcitx \
             mozc \
             clang \
-            psensor'
+            psensor"
 
-SNAPS='htop'
-SNAPS_CL='slack'
+SNAPS="htop"
 
 sudo apt update -y && sudo apt upgrade -y
 sudo snap refresh
@@ -18,10 +17,15 @@ sudo snap set system refresh.timer=24:00
 
 
 if [ "$1" == "all" ]; then
-    echo -e "\033[0;33minstall all\033[0;39m"
-    sudo apt install -y $ESSENTIAlS $ADDITIONALS
-    sudo snap install $SNAPS
-    sudo snap install $SNAPS_CL --classic
+
+    # WSL 2 specific settings.
+    if grep -q "microsoft" /proc/version &>/dev/null; then
+        echo -e "\033[0;33mWSL setting\033[0;39m"
+    else
+        echo -e "\033[0;33Linux setting\033[0;39m"
+        sudo apt install -y $ESSENTIAlS $ADDITIONALS
+        sudo snap install $SNAPS
+    fi
 else
     echo -e "\033[0;35minstall essentials\033[0;39m"
     sudo apt install -y $ESSENTIAlS
